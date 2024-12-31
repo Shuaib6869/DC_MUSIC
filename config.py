c4883949b5b3201a46f47b7b23c088031cb82797
Shuaib6869 <Suvebansari820@gmail.com>
2024-12-31 04:49:07 -0800

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/istkharalam6202/DC_MUSIC",
+    "https://github.com/Shuaib6869/DC_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
